ab8d187cf3b7c71064890f8101377e8e75209bad
gmiashiro <g.miashiro.freitas@gmail.com>
2025-07-04 17:28:19 -0300

    primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    
+  </header>
+
+  <main>
+    
+  </main>
+</template>
+
+<style>
+
+</style>
